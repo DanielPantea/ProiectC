b92451e806a3cf0f823e3317011621ee949391dd
Student <student@debian.localdomain>
2019-11-08 13:15:51 +0200

    Am facut functia pt ordonare cresc

--- alex.c
+++ alex.c
@@ -1,1 +1,21 @@
 #include<stdio.h>
+void sort_et_pret_cresc(int v[101],int &n)
+{
+bool s;
+int i,aux
+ do{
+        s = false;
+        for (i = 1; i < n; i++){
+            if (v[i - 1] > v[i]){
+                aux = v[i - 1];
+                v[i - 1] = v[i];
+                v[i] = aux;
+                s = true;
+            }
+        }
+    } while (s);
+    printf("Preturile ordonate crescatot pe etajul %d sunt \n", n);
+    for(i=0;i<n;i++)
+     printf("Persoana %d are taxa de %d lei \n",i,v[i]);
+
+}
