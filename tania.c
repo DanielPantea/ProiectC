9f1e4b1604a2af478fe3f1d524bf900609de0f9d
Student <student@b418a-6.localdomain>
2019-11-15 13:11:57 +0200

    Am adagat tania c

--- tania.c
+++ tania.c
@@ -2,11 +2,30 @@
 
 float media_et(int m[101], int p)
 {
- int i; float md=0;
- for(i=0;i<p;i++)
-    md=md+m[i];
+    int i;
+    float md=0;
+    for(i=0; i<p; i++)
+        md=md+m[i];
 
- return md/p;
+    return md/p;
+
+}
+
+float max_et(int m[11][101],int p)
+{
+
+    int i;
+    float maxi=0;
+    for(i=0; i<et; i++)
+    {
+        sumet=0;
+        for(j=0; j<p; j++)
+            sumet=sumet+m[i][j];
+
+        if (sumet>maxi) maxi=sumet;
+    }
+
+    return maxi;
 
 }
 
--- tania.h
+++ tania.h
@@ -4,4 +4,6 @@
 ///like this one, from the original c file (tania.c)
 float media_et(int m[101], int p);
 
+float max_et(int m[11][101],int p);
+
 #endif // TANIA_H_INCLUDED
